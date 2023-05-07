3b4cd4842d733c77228a061f28103a628fdab1ec
narayanshettigar <117894336+narayanshettigar@users.noreply.github.com>
2023-05-07 12:33:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+var audio = null;
+
+function playSound(filename) {
+  if (audio !== null) {
+    stopSound();
+  }
+  audio = new Audio("sounds/" + filename);
+  audio.play();
+}
+
+function stopSound() {
+  if (audio !== null) {
+    audio.pause();
+    audio.currentTime = 0;
+    audio = null;
+  }
+}
